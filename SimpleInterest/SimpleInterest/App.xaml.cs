9aa2497d15e6cd40c4bca62365ba02c2a84d7962
Aravind997 <aravindg997@gmail.com>
2022-01-07 00:23:47 +0530

    Add project files.

---
+++ SimpleInterest/SimpleInterest/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SimpleInterest.Services;
+using SimpleInterest.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleInterest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new SimpleInterestCalculator();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SimpleInterest/SimpleInterest/Views/SimpleInterestCalculator.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleInterest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SimpleInterestCalculator : ContentPage
+        
+    {
+        
+        public SimpleInterestCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Clicked(object sender, EventArgs e)
+        {
+            float p = float.Parse(TextBox_PrincipleAmount.Text);
+            float n = float.Parse(TextBox_NumberofYears.Text);
+            float r = float.Parse(TextBox_RateofInterest.Text);
+            float s = ((p * n * r) /100);
+            TextBox_InterestAmount.Text = s.ToString();
+            float t = (s + p);
+            TextBox_TotalAmount.Text = t.ToString();
+            
+        }
+
+        private void btnCancel_Clicked(object sender, EventArgs e)
+        {
+            TextBox_PrincipleAmount.Text = "0";
+            TextBox_NumberofYears.Text = "0";
+            TextBox_RateofInterest.Text = "0";
+            TextBox_InterestAmount.Text = "0";
+            TextBox_TotalAmount.Text = "0";
+        }
+    }
+}
